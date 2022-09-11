e85d1bba88c581820cd476bf2ad925064e27edd7
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-11 09:47:53 +0900

    Devlop ブランチに変更を追加

--- 1-6/Program.cs
+++ 1-6/Program.cs
@@ -6,13 +6,6 @@ class Program
     static void Main(string[] args)
     {
         int[] array = { 1, -1, 2, -2, 3 };
-        foreach(var item in array)
-        {
-            if (item < 0)
-            {
-                Console.WriteLine(item);
-                break;
-            }
-        }
+        Console.WriteLine(array.FirstOrDefault(c=> c<0));
     }
 }
