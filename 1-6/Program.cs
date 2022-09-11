93184faf7f5503dc6394a7f875be7223792b62fe
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-11 09:43:40 +0900

    1-6 project 追加

---
+++ 1-6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, -1, 2, -2, 3 };
+        foreach(var item in array)
+        {
+            if (item < 0)
+            {
+                Console.WriteLine(item);
+                break;
+            }
+        }
+    }
+}
