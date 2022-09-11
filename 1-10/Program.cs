6d473e7f69d9ecfa0ace9187c9c77a6ec0531cb3
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-11 17:33:49 +0900

    Developブランチに1-10の変更をコミット

---
+++ 1-10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Submarine
+{
+    public int Males { get; private set; }
+    public Submarine(int n)
+    {
+        Males = n;
+    }
+}
+class Cruiser
+{
+    public int Males { get; private set; }
+    public int Females { get; private set; }
+    public Cruiser(int m,int f)
+    {
+        Males=m;
+        Females=f;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        object[] list = { new Submarine(30), new Cruiser(10, 20) };
+        int sum = 0;
+        foreach (dynamic item in list)
+        {
+            sum += item.Females;
+        }
+        Console.WriteLine("Males in ship are " + sum);
+    }
+}
