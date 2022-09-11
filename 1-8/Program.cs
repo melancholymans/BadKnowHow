eef7be85ecb394397ca407b4132e6fe6c642870d
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-11 17:06:16 +0900

    Out of Memoryの解決策

---
+++ 1-8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class SimpleSum
+{
+    private int[] array;
+    private int sum;
+    
+    private void calc()
+    {
+        sum = array.Sum();
+    }
+    public SimpleSum(int max)
+    {
+        array = Enumerable.Range(0,max).ToArray();
+        calc();
+        array = null;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new List<SimpleSum>();
+        for (int i= 0; i < 1000000; i++)
+        {
+            list.Add(new SimpleSum(10000));
+        }
+        Console.WriteLine("the End");
+    }
+}
